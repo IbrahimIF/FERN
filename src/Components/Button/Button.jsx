737fa7763c2f31d433a9a5ecad361065721a8ed8
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-12-06 20:41:24 +0000

    Upload of mern project files
    Uploaded all files from my mern project for fern project

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Button.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpload }  from '@fortawesome/free-solid-svg-icons';
+
+
+
+function Button({onDataSent}) {
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/api/Reacr-MongoDB', { //https://react-to-mongodb.vercel.app/ //http://localhost:4000/Reacr-MongoDB
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, text })
+    })
+    .then(response => response.json())
+    .then(newData => { onDataSent(); });
+  };
+
+
+  return (
+    <>
+    
+    <div className="buttonContainer">
+      <form onSubmit={handleSubmit}>
+        <div className="inputContainer">
+          <div className="input-Animated">
+            <input type="text" name="text" className="input" placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            />
+            <div className="highlight"></div>
+          </div>
+
+          <div className="input-Animated">
+            <input type="text" name="text" className="input" placeholder="Enter Text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            />
+            <div className="highlight"></div>
+          </div>
+
+        </div>
+        <div className="submitButtonContainer">
+        <button type="submit"> <FontAwesomeIcon icon={faUpload} /> &nbsp; Send </button>
+        </div>
+      </form>
+    </div>
+    </>
+  );
+}
+
+export default Button;
+
+
